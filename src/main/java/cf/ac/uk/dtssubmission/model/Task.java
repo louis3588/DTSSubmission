defbb67bd1e2b519c24b304808ac61171887850b
c21079342 <pollyl@cardiff.ac.uk>
2025-04-21 14:49:01 +0100

    modelled task object

--- src/main/java/cf/ac/uk/dtssubmission/model/Task.java
+++ src/main/java/cf/ac/uk/dtssubmission/model/Task.java
@@ -1,6 +1,23 @@
 package cf.ac.uk.dtssubmission.model;
 
-//Model structure for Task
-public class Task {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+
+//Model structure for Task
+@Entity
+@Getter
+@Setter
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+    private LocalDateTime dueDateTime;
 }
